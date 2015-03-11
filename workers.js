749c98cdc3b1654adf18dd7d83866e18423f8787
Jerson Carranza Lucas <jerson_carranza2@hotmail.com>
2015-03-11 01:40:10 -0500

    configuracion

--- workers.js
+++ workers.js
@@ -1,6 +1,6 @@
 var http	= require('http');
 var conf = require('./conf.json');
-var expressServer = require('./app/ExpressServergit commit.js');
+var expressServer = require('./app/ExpressServer');
 var mongoose = require('mongoose');
 var socketIO= require('./app/socketIO.js');
  
@@ -9,6 +9,7 @@ var Workers = function (config) {
 config = config || {}
 
 mongoose.connect('mongodb://' +conf.db.user +':'+ conf.db.password + conf.db.host +'/'+ conf.db.name);
+//mongodb://nodejitsu:3af9571979d1c5269b1208be54bfa38f@troup.mongohq.com:10073/nodejitsudb3726992572
 var app = new expressServer();
 
 
@@ -18,7 +19,7 @@ var Io = new socketIO({server:server});
 server.listen(conf.port);
 console.log('escuchando el puerto 7000');
 }
-virginia 23 diciembre
+/*virginia 23 diciembre*/
 
 //para poder debuggearlos
 if (module.parent){//es para llamarlo al padre 
